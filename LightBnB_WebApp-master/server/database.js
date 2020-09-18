fced736879efbeca03261f7f3599acd8662e058e
zainab66 <zainab66r@yahoo.com>
2020-09-18 01:53:33 +0000

    add login,sigup and logout

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -17,16 +17,10 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  return pool.query(`SELECT * FROM users
+  WHERE email = $1;`,[email])
+  .then(res => res.rows[0])
+  .catch(rej => null);
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -36,7 +30,10 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`
+  SELECT * FROM users
+  WHERE id = $1;`, [id])
+  .then(res => res.rows[0]);
 }
 exports.getUserWithId = getUserWithId;
 
@@ -47,10 +44,10 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool.query (`INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;`, [user.name, user.email, user.password])
+  .then(res => res.rows[0] )
 }
 exports.addUser = addUser;
 
